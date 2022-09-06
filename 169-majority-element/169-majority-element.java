1b67841d7e0acfb1e0f592d58ffd8ee0f613efe4
Tanmay Patil <108663700+Tanmay-Patil-08@users.noreply.github.com>
2022-09-06 19:51:13 +0530

    Time: 2 ms (89.80%), Space: 56.6 MB (30.26%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,6 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int count = 0 ; 
+        int maxElement = 0;
+        
+        for(int num : nums ){
+            if (count == 0 ){
+                maxElement = num;
+                count++;
+            }else{
+                if(num == maxElement){
+                    count++;
+                }
+                else{
+                    count--;
+                }
+            }
+        }
+        return maxElement;
+        
     }
 }
